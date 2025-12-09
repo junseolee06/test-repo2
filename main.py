9f5ce456dc54450f0bf71bec5ea36b7493391c00
junseolee06 <jslee11833@gmail.com>
2025-12-09 14:20:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-first = input("enter the first number")
-second = input("enter the second number")
+first = input("Enter the first number")
+second = input("Enter the second number")
 
 num1 = int(first)
 num2 = int(second)
 
-result = num1 + num2
-
-print("Sum",result)
+sum_result = num1 + num2
+sub_result = num1 - num2
+mul_result = num1 * num2
+print("Sum: ",sum_result)
+print("Difference: ", sub_result)
+print("Product: ", mul_result)
